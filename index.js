a3d686b45c5e12c3dd964a3e76587f87896b33c2
Harman-jot-Kaur <harmanjotkaurr01@gmail.com>
2026-02-05 19:21:15 -0600

    Update message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello! This is Version 1 of the Express App.</h1>");
+  res.send(" <h1>Version 2: Deployed Automatically!</h1>");
 });
 
 app.listen(port, () => {
